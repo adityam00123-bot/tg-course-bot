f8a47dba2f6eacef755a66600dc53ebe7021a9e3
Aditya Mishra <tradezone1104@gmail.com>
2026-08-29 13:18:44 +0530

    clean: remove web_course_downloader folder to keep repo 100% dedicated to TG Migration Bot

--- web_course_downloader/extractors/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Extractors package for Web & LMS Course Downloader.
-"""
-from .classplus import ClassplusAPI
-from .generic_stream import GenericStreamExtractor
-
-__all__ = ["ClassplusAPI", "GenericStreamExtractor"]
